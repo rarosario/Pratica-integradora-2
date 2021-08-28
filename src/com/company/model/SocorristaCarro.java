3147cec1049319a6354ff0f1a5bc97ce8eb2a06b
rarosario <rafael.rosario@mercadolivre.com>
2021-08-28 14:30:11 -0300

    Nova Atualização

--- src/com/company/model/SocorristaCarro.java
+++
@@ -1,8 +0,0 @@
-package com.company.model;
-
-public class SocorristaCarro extends Veiculo{
-
-    public void socorrer(Carro carro){
-
-    }
-}
--- src/com/company/model/SocorristaMoto.java
+++
@@ -1,8 +0,0 @@
-package com.company.model;
-
-public class SocorristaMoto extends Veiculo{
-
-    public void socorrer(Moto moto){
-
-    }
-}
