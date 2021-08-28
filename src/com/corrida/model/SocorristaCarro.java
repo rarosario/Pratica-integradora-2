bf91b3498187b876d92553bdff5bb19ee0715740
rarosario <rafael.rosario@mercadolivre.com>
2021-08-28 14:24:34 -0300

    Atualização

---
+++ src/com/corrida/model/SocorristaCarro.java
@@ -0,0 +1,9 @@
+package com.corrida.model;
+
+public class SocorristaCarro {
+
+    public void socorrer(Carro carro) {
+        System.out.println("Soccorrendo carro =>" + carro.getPlaca());
+    }
+
+}
---
+++ src/com/corrida/model/SocorristaMoto.java
@@ -0,0 +1,8 @@
+package com.corrida.model;
+
+public class SocorristaMoto {
+
+    public void socorrer(Moto moto) {
+        System.out.println("Soccorrendo moto =>" + moto.getPlaca());
+    }
+}
